59be7b8892d893f8603974adb699b291ab056b0c
renancarranza1920 <128913758+renancarranza1920@users.noreply.github.com>
2025-04-13 16:16:23 -0600

    semilla usuario

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Administrador',
             'email' => 'admin@gmail.com',
-            'password' => Hash::make('admin123'),
+            'password' => Hash::make('admin'),
         ]);
        
     }
