e74b388e665685f011dd130255b9e23d0c6145ab
manuel <manuenrike@gmail.com>
2025-04-15 16:05:47 -0600

    storage 15/04/25

--- storage/storage/framework/views/5e1c83e2de93d27cb0120d7cd2f99303.php
+++
@@ -1,5 +0,0 @@
-
-
-        <img 
-        src="<?php echo e(asset('storage/logob.png')); ?>" 
-        alt="Logo Claro" ><?php /**PATH C:\xampp\htdocs\lameri\resources\views/vendor/filament-panels/components/logo-oscuro.blade.php ENDPATH**/ ?>
--- storage/storage/framework/views/8ca1b3b7a50edaced106986775ade4bb.php
+++
@@ -1,7 +0,0 @@
-
-    <img 
-        src="<?php echo e(asset('storage/logow.png')); ?>" 
-        alt="Logo Claro" >
-
-
-     <?php /**PATH C:\xampp\htdocs\lameri\resources\views/vendor/filament-panels/components/logo-claro.blade.php ENDPATH**/ ?>
