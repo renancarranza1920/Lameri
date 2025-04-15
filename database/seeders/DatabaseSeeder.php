f385495058163656220c922c1ff6750ac7e010bb
manuel <manuenrike@gmail.com>
2025-04-15 15:59:04 -0600

    Seeders combinado

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,6 +25,45 @@ class DatabaseSeeder extends Seeder
             'password' => Hash::make('admin'),
         ]);
 
+        cliente::insert([
+            [
+            'NumeroExp' => 'EA25001',
+            'nombre' => 'Erick Eduardo',
+            'apellido' => 'Alonzo Dominguez',
+            'fecha_nacimiento' => '1995-05-15',
+            'telefono' => '1234567890',
+            'correo' => 'ad18017@ues.edu.sv',
+            'direccion' => 'Calle Falsa 123',
+            'estado' => 'Activo',
+            'created_at' => now(),
+            'updated_at' => now(),
+            ],
+            [
+                'NumeroExp' => 'RC25001',
+                'nombre' => 'Renan Gilberto',
+                'apellido' => 'Carranza Estupinian',
+                'fecha_nacimiento' => '2000-06-15',
+                'telefono' => '0987654321',
+                'correo' => 'ce18008@ues.edu.sv',
+                'direccion' => 'Avenida Principal 456',
+                'estado' => 'Activo',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'NumeroExp' => 'MD25001',
+                'nombre' => 'Manuel Enrique',
+                'apellido' => 'Dominguez Lopez',
+                'fecha_nacimiento' => '2010-06-15',
+                'telefono' => '1234567877',
+                'correo' => 'ad14017@ues.edu.sv',
+                'direccion' => 'Calle Secundaria 789',
+                'estado' => 'Activo',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        ]);
+
         // Lista ordenada alfabéticamente y con tildes
         $tipos = [
             'Bacteriología',
