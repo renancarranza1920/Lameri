08374ff897a30df210c85df02577514c2a637426
manuel <manuenrike@gmail.com>
2025-08-23 13:43:31 -0600

    view examens corregido 23-8-25

--- app/Filament/Resources/ExamenResource.php
+++ app/Filament/Resources/ExamenResource.php
@@ -191,7 +191,6 @@ class ExamenResource extends Resource
             'index' => Pages\ListExamens::route('/'),
             'create' => Pages\CreateExamen::route('/create'),
             'edit' => Pages\EditExamen::route('/{record}/edit'),
-            'view' => Pages\ViewExamen::route('/{record}'),
         ];
     }
 }
