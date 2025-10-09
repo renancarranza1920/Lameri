9e388f8f0f27bc11e8ae85a080e188b512cea32d
renancarranza1920 <ce18008@ues.edu.sv>
2025-10-09 15:40:41 -0600

    añadiendo lo de lalo

--- app/Services/ZebraLabelService.php
+++ app/Services/ZebraLabelService.php
@@ -81,15 +81,23 @@ class ZebraLabelService
             $recipiente = \App\Enums\RecipienteEnum::tryFrom($color)?->getTitle();
         }
 
+        Log::info("Generando etiquetas ZPL para recipiente: {$recipiente}, con " . count($items) . " exámenes.");
         // Fecha actual en formato dd/mm/yyyy
         $fecha = date('d/m/Y');
         $hora = date('h:i A');
 
         $examenesUnicos = collect();
+        $examenesSecereciones = collect();
         foreach ($items as $detalle) {
-            if ($detalle->examen && $detalle->examen->tipoExamen) {
+           
+            if($detalle->status == "cultivo_secreciones"){
+                $examenesSecereciones->push($detalle->examen->nombre);
+            }else{
+                if ($detalle->examen && $detalle->examen->tipoExamen) {
                 $examenesUnicos->push($detalle->examen->tipoExamen->nombre);
             }
+            }
+            
         }
         $examenesUnicos = $examenesUnicos->unique()->values();
 
@@ -99,21 +107,22 @@ class ZebraLabelService
         $zpl = '';
 
         foreach ($bloques as $bloque) {
-            $examenLines = '';
-            $startY = 135;
-            $lineHeight = 25;
+    $examenLines = '';
+    $startY = 135;
+    $lineHeight = 25;
 
-            foreach ($bloque as $index => $examen) {
-                $examenTexto = strtoupper(substr($examen, 0, 16));
-                $col = $index % 2; // 0 for first column, 1 for second
-                $row = floor($index / 2);
-                $posX = $col === 0 ? 30 : 200; // X position for column 1 and 2
-                $posY = $startY + ($lineHeight * $row);
-                $examenLines .= "^FO{$posX},{$posY}^ADN,8,4^FD-{$examenTexto}^FS\n";
-            }
+    foreach ($bloque->values() as $index => $examen) {
+        $examenTexto = strtoupper(substr($examen, 0, 16));
+        $col = $index % 2; // 0 = primera columna, 1 = segunda
+        $row = intdiv($index, 2); // fila dentro del bloque
+        $posX = $col === 0 ? 30 : 200;
+        $posY = $startY + ($lineHeight * $row);
 
-            // Plantilla fija de la etiqueta
-            $zpl .= "^XA
+        $examenLines .= "^FO{$posX},{$posY}^ADN,8,4^FD-{$examenTexto}^FS\n";
+    }
+
+    // Plantilla fija de la etiqueta
+    $zpl .= "^XA
 ^PW406
 ^LL203
 ^CI28 
@@ -125,7 +134,26 @@ class ZebraLabelService
 ^FO20,120^GB366,1,1^FS
 {$examenLines}
 ^XZ\n\n";
-        }
+}
+
+    foreach ($examenesSecereciones->values() as $examen) {
+    $examenTexto = strtoupper(substr($examen, 0, 30));
+    $posX = 30;
+    $posY = 135;
+
+    $zpl .= "^XA
+^PW406
+^LL203
+^CI28 
+^FO5,5^GB396,193,2^FS
+^FO20,20^A0N,28,28^FDLaboratorio Merino^FS
+^FO20,50^A0N,18,18^FDPaciente: {$paciente}^FS
+^FO20,75^A0N,18,18^FDRecipiente: {$recipiente}^FS
+^FO20,100^A0N,18,18^FDFecha y Hora: {$fecha} {$hora}^FS
+^FO20,120^GB366,1,1^FS
+^FO30,135^A0N,18,18^FD -{$examenTexto}
+^XZ\n\n";
+}
 
         return $zpl;
     }
